6fdfd84990b12146264538d600915556a10117d1
Nobody <bunnza2020@gmail.com>
2024-07-29 04:43:43 -0500

    Sorting done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 #.file Endings
 audio_files = (".mp3",".wav",".flac",".aac",".ogg",".wma")
@@ -9,9 +10,23 @@ text_files = (".txt",".doc",".pdf",".rtf",".odt")
 Messy_dir_path = r"" #Put full file path
 Files = os.listdir(Messy_dir_path)
 
-audio_folder = ""
-video_folder = ""
-image_folder = ""
-text_folder = ""
+audio_folder = r""
+video_folder = r""
+image_folder = r""
+text_folder = r""
+
+#shutil.move(source, destination)
 
 
+for file in Files:
+    index = file.find(".")
+    Des_folder = ""
+    if index == -1:continue
+    if file[index:] in audio_files: Des_folder = audio_folder
+    elif file[index:] in video_files: Des_folder = video_folder
+    elif file[index:] in image_files: Des_folder = image_folder
+    elif file[index:] in text_files: Des_folder = text_folder
+    else:continue
+    shutil.move(f"{Messy_dir_path}\\{file}",Des_folder)
+
+print("All Sorted")
