ec960a18605042384c5982e5a7fd15053e601339
Nobody <bunnza2020@gmail.com>
2024-07-30 04:27:22 -0500

    Almost

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template,request
-
+from flask import Flask,render_template,request,jsonify
+from File import Sort_Files
 
 
 
@@ -13,7 +13,11 @@ app = Flask(__name__,template_folder="templates")
 def index():
     return render_template("index.html")
 
-
+@app.route("/Confirm",methods=["post"])
+def Confirm():
+    data = request.form
+    Sort_Files(data.get("SortingFile"),data.get("AudioFile"),data.get("VideoFile"),data.get("ImageFile"),data.get("TextFile"))
+    return jsonify(data)
 
 
 
--- File.py
+++ File.py
@@ -7,24 +7,15 @@ video_files = (".mpg",".mov",".wmv",".rm",".webm",".mp4")
 image_files = (".jpeg",".png",".svg",".gif")
 text_files = (".txt",".doc",".pdf",".rtf",".odt")
 
-Messy_dir_path = r"" #Put full file path
-Files = os.listdir(Messy_dir_path)
-
-audio_folder = r""
-video_folder = r""
-image_folder = r""
-text_folder = r""
-
-
-for file in Files:
-    index = file.find(".")
-    Des_folder = ""
-    if index == -1:continue
-    if file[index:] in audio_files: Des_folder = audio_folder
-    elif file[index:] in video_files: Des_folder = video_folder
-    elif file[index:] in image_files: Des_folder = image_folder
-    elif file[index:] in text_files: Des_folder = text_folder
-    else:continue
-    shutil.move(f"{Messy_dir_path}\\{file}",Des_folder)
-
-print("All Sorted")
+def Sort_Files(Messy_dir_path,audio_folder,video_folder,image_folder,text_folder):
+    Files = os.listdir(Messy_dir_path)
+    for file in Files:
+        index = file.find(".")
+        Des_folder = ""
+        if index == -1:continue
+        if file[index:] in audio_files: Des_folder = audio_folder
+        elif file[index:] in video_files: Des_folder = video_folder
+        elif file[index:] in image_files: Des_folder = image_folder
+        elif file[index:] in text_files: Des_folder = text_folder
+        else:continue
+        shutil.move(f"{Messy_dir_path}\\{file}",Des_folder)
